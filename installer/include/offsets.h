4f6e3dfcd4c13f7ebf6866b77731c25db4e3eb95
Simone Ruggieri <simopich2002@gmail.com>
2024-06-01 17:20:12 +0200

    Added offsets generated by BestPig

--- installer/include/offsets.h
+++ installer/include/offsets.h
@@ -3,13 +3,13 @@
 #pragma once
 
 // 5.05
-#define	XFAST_SYSCALL_addr              0x00001C0
+#define	XFAST_SYSCALL_addr              0x000001C0 // Done
 
 // Names - Data
 #define PRISON0_addr                    0x10986A0
-#define ROOTVNODE_addr                  0x22C1A70
-#define PMAP_STORE_addr                 0x22CB570
-#define DT_HASH_SEGMENT_addr            0x0B5EF30
+#define ROOTVNODE_addr                  0x021ebf20 // Done
+#define PMAP_STORE_addr                 0x01b8c4b0 // Done
+#define DT_HASH_SEGMENT_addr            0x00140fe0 // Done
 
 // Functions
 #define pmap_protect_addr               0x02E3090
@@ -21,11 +21,11 @@
 #define debug_menu_error_patch2         0x04FA15C
 
 // disable signature check
-#define disable_signature_check_patch   0x06A2700
+#define disable_signature_check_patch   0x00686580 // Done
 
 // enable debug RIFs
-#define enable_debug_rifs_patch1        0x064B2B0
-#define enable_debug_rifs_patch2        0x064B2D0
+#define enable_debug_rifs_patch1        0x0064e3f0 // Done
+#define enable_debug_rifs_patch2        0x0064e420 // Done
 
 // allow sys_dynlib_dlsym in all processes
 #define sys_dynlib_dlsym_patch          0x0237F3A
@@ -34,9 +34,9 @@
 #define sdk_version_patch               0x14A63F0
 
 // enable debug log
-#define enable_debug_log_patch          0x043612A
+#define enable_debug_log_patch          0x000b7ac7 // Done
 
 // enable uart output
-#define enable_uart_patch               0x19ECEB0
+#define enable_uart_patch               0x01527f60 // Done
 
 #endif
--- kpayload/include/offsets.h
+++ kpayload/include/offsets.h
@@ -7,77 +7,77 @@
 #define M_TEMP_addr                     0x0155E1E0 // Done
 #define MINI_SYSCORE_SELF_BINARY_addr   0x14C9D48
 #define ALLPROC_addr                    0x01B906E0 // Done
-#define SBL_DRIVER_MAPPED_PAGES_addr    0x271E208
-#define SBL_PFS_SX_addr                 0x271E5D8
-#define SBL_KEYMGR_KEY_SLOTS_addr       0x2744548
-#define SBL_KEYMGR_KEY_RBTREE_addr      0x2744558
-#define SBL_KEYMGR_BUF_VA_addr          0x2748000
-#define SBL_KEYMGR_BUF_GVA_addr         0x2748800
-#define FPU_CTX_addr                    0x274C040
-#define DIPSW_addr                      0x1CD0650
+#define SBL_DRIVER_MAPPED_PAGES_addr    0x02642ca8 // Done
+#define SBL_PFS_SX_addr                 0x02649b40 // Done
+#define SBL_KEYMGR_KEY_SLOTS_addr       0x02644238 // Done
+#define SBL_KEYMGR_KEY_RBTREE_addr      0x02644248 // Done
+#define SBL_KEYMGR_BUF_VA_addr          0x02648000 // Done
+#define SBL_KEYMGR_BUF_GVA_addr         0x02648808 // Done
+#define FPU_CTX_addr                    0x026735c0 // Done
+#define DIPSW_addr                      0x02212850 // Done
 
 // common
-#define memcmp_addr                     0x050AC0
-#define _sx_xlock_addr                  0x0F5E10
-#define _sx_xunlock_addr                0x0F5FD0
+#define memcmp_addr                     0x00271aa0 // Done
+#define _sx_xlock_addr                  0x0043c530 // Done
+#define _sx_xunlock_addr                0x0043c6f0 // Done
 #define malloc_addr                     0x003017B0 // Done
 #define free_addr                       0x00301970 // Done
-#define strstr_addr                     0x17DFB0
-#define fpu_kern_enter_addr             0x1BFF90
-#define fpu_kern_leave_addr             0x1C0090
-#define memcpy_addr                     0x1EA530
-#define memset_addr                     0x3205C0
-#define strlen_addr                     0x3B71A0
-#define printf_addr                     0x436040
-#define eventhandler_register_addr      0x1EC400
+#define strstr_addr                     0x004859b0 // Done
+#define fpu_kern_enter_addr             0x002193a0 // Done
+#define fpu_kern_leave_addr             0x00219460 // Done
+#define memcpy_addr                     0x00271130 // Done
+#define memset_addr                     0x00149670 // Done
+#define strlen_addr                     0x0030f0f0 // Done
+#define printf_addr                     0x000b79e0 // Done
+#define eventhandler_register_addr      0x000f8320 // Done
 
 // Fself
-#define sceSblACMgrGetPathId_addr       0x0117E0
-#define sceSblServiceMailbox_addr       0x632540
-#define sceSblAuthMgrSmIsLoadable2_addr 0x63C4F0
-#define _sceSblAuthMgrGetSelfInfo_addr  0x63CD40
-#define _sceSblAuthMgrSmStart_addr      0x6418E0
-#define sceSblAuthMgrVerifyHeader_addr  0x642B40
+#define sceSblACMgrGetPathId_addr       0x0008bcd0 // Done
+#define sceSblServiceMailbox_addr       0x0062ec00 // Done
+#define sceSblAuthMgrSmIsLoadable2_addr 0x00641960 // Done
+#define _sceSblAuthMgrGetSelfInfo_addr  0x006421a0 // Done
+#define _sceSblAuthMgrSmStart_addr      0x0063dea0 // Done
+#define sceSblAuthMgrVerifyHeader_addr  0x0063fc20 // Done
 
 // Fpkg
 #define RsaesPkcs1v15Dec2048CRT_addr    0x1FD7D0
-#define Sha256Hmac_addr                 0x2D55B0
-#define AesCbcCfb128Encrypt_addr        0x3A2BD0
-#define AesCbcCfb128Decrypt_addr        0x3A2E00
-#define sceSblDriverSendMsg_0_addr      0x61D7F0
-#define sceSblPfsSetKeys_addr           0x61EFA0
-#define sceSblKeymgrSetKeyStorage_addr  0x623FC0
-#define sceSblKeymgrSetKeyForPfs_addr   0x62D780
-#define sceSblKeymgrCleartKey_addr      0x62DB10
-#define sceSblKeymgrSmCallfunc_addr     0x62E2A0
+#define Sha256Hmac_addr                 0x00442f80 // Done
+#define AesCbcCfb128Encrypt_addr        0x001ff000 // Done
+#define AesCbcCfb128Decrypt_addr        0x001ff230 // Done
+#define sceSblDriverSendMsg_0_addr      0x0061ae90 // Done
+#define sceSblPfsSetKeys_addr           0x00623290 // Done
+#define sceSblKeymgrSetKeyStorage_addr  0x00622930 // Done
+#define sceSblKeymgrSetKeyForPfs_addr   0x0061d650 // Done
+#define sceSblKeymgrCleartKey_addr      0x0061d990 // Done
+#define sceSblKeymgrSmCallfunc_addr     0x0061d220 // Done
 
 // Patch
-#define vmspace_acquire_ref_addr        0x19EF90
-#define vmspace_free_addr               0x19EDC0
+#define vmspace_acquire_ref_addr        0x0007b9e0 // Done
+#define vmspace_free_addr               0x0007b810 // Done
 #define vm_map_lock_read_addr           0x0007BB80 // Done
 #define vm_map_unlock_read_addr         0x0007BBD0 // Done
 #define vm_map_lookup_entry_addr        0x0007C1C0 // Done
-#define proc_rwmem_addr                 0x30D150
+#define proc_rwmem_addr                 0x0041ca70 // Done
 
 // Fself hooks
-#define sceSblAuthMgrIsLoadable__sceSblACMgrGetPathId_hook          0x63E25D
+#define sceSblAuthMgrIsLoadable__sceSblACMgrGetPathId_hook          0x006426fc // Done
 #define sceSblAuthMgrIsLoadable2_hook                               0x63E3A1
-#define sceSblAuthMgrVerifyHeader_hook1                             0x63EAFC
+#define sceSblAuthMgrVerifyHeader_hook1                             0x00642fe6 // Done
 #define sceSblAuthMgrVerifyHeader_hook2                             0x63F718
-#define sceSblAuthMgrSmLoadSelfSegment__sceSblServiceMailbox_hook   0x64318B
+#define sceSblAuthMgrSmLoadSelfSegment__sceSblServiceMailbox_hook   0x006402ea // Done
 #define sceSblAuthMgrSmLoadSelfBlock__sceSblServiceMailbox_hook     0x643DA2
 
 // Fpkg hooks
-#define sceSblKeymgrSetKeyStorage__sceSblDriverSendMsg_hook         0x624065
+#define sceSblKeymgrSetKeyStorage__sceSblDriverSendMsg_hook         0x006229d5 // Done
 #define sceSblKeymgrInvalidateKey__sx_xlock_hook                    0x62E96D
-#define sceSblKeymgrSmCallfunc_npdrm_decrypt_isolated_rif_hook      0x64C720
+#define sceSblKeymgrSmCallfunc_npdrm_decrypt_isolated_rif_hook      0x0064c030 // Done
 #define sceSblKeymgrSmCallfunc_npdrm_decrypt_rif_new_hook           0x64D4FF
 #define mountpfs__sceSblPfsSetKeys_hook1                            0x6AAAD5
 #define mountpfs__sceSblPfsSetKeys_hook2                            0x6AAD04
 
 // SceShellUI patches - debug patches
-#define sceSblRcMgrIsAllowDebugMenuForSettings_patch                0x01BD90
-#define sceSblRcMgrIsStoreMode_patch                                0x01C090
+#define sceSblRcMgrIsAllowDebugMenuForSettings_patch                0x0001D1C0 // Done - Based on name only
+#define sceSblRcMgrIsStoreMode_patch                                0x0001D520 // Done - Based on name only
 
 // SceShellUI patches - remote play patches
 #define CreateUserForIDU_patch                                      0x1A8FA0
@@ -95,10 +95,10 @@
 #define sceKernelIsGenuineCEX_patch4    0x00A2A254 // Done
 
 // call nidf_libSceDipsw
-#define nidf_libSceDipsw_patch1         0x0016F042 // Done - Maybe not???
-#define nidf_libSceDipsw_patch2         0x0024A4EB // Done - Maybe not???
-#define nidf_libSceDipsw_patch3         0x00864772 // Done - Maybe not???
-#define nidf_libSceDipsw_patch4         0x00A2A282 // Done - Maybe not???
+#define nidf_libSceDipsw_patch1         0x0016F042 // Done - Based on name only
+#define nidf_libSceDipsw_patch2         0x0024A4EB // Done - Based on name only
+#define nidf_libSceDipsw_patch3         0x00864772 // Done - Based on name only
+#define nidf_libSceDipsw_patch4         0x00A2A282 // Done - Based on name only
 
 // enable data mount
 #define enable_data_mount_patch         0x00321F2B // Done
