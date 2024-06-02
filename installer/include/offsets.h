f233d18a0e00fa41fc5413046b77f72ef1ebca4b
Simone Ruggieri <simopich2002@gmail.com>
2024-06-02 12:54:52 +0200

    Ported installer offsets

--- installer/include/offsets.h
+++ installer/include/offsets.h
@@ -6,19 +6,20 @@
 #define	XFAST_SYSCALL_addr              0x000001C0 // Done
 
 // Names - Data
-#define PRISON0_addr                    0x10986A0
+#define PRISON0_addr                    0x0111b840 // Done
 #define ROOTVNODE_addr                  0x021ebf20 // Done
 #define PMAP_STORE_addr                 0x01b8c4b0 // Done
 #define DT_HASH_SEGMENT_addr            0x00140fe0 // Done
 
 // Functions
-#define pmap_protect_addr               0x02E3090
-#define pmap_protect_p_addr             0x02E30D4
+#define pmap_protect_addr               0x0012fa40 // Done
+#define pmap_protect_p_addr             0x0012fa87 // Done
+#define malloc_addr                     0x003017B0 // Done
 
 // Patches
 // debug menu error
-#define debug_menu_error_patch1         0x04F9048
-#define debug_menu_error_patch2         0x04FA15C
+#define debug_menu_error_patch1         0x004e6d48 // Done
+#define debug_menu_error_patch2         0x004e802f // Done - Not 100% sure
 
 // disable signature check
 #define disable_signature_check_patch   0x00686580 // Done
@@ -28,10 +29,10 @@
 #define enable_debug_rifs_patch2        0x0064e420 // Done
 
 // allow sys_dynlib_dlsym in all processes
-#define sys_dynlib_dlsym_patch          0x0237F3A
+#define sys_dynlib_dlsym_patch          0x0023b34f // Done
 
 // sdk version spoof - enable all VR fws
-#define sdk_version_patch               0x14A63F0
+// #define sdk_version_patch               0x14A63F0
 
 // enable debug log
 #define enable_debug_log_patch          0x000b7ac7 // Done
