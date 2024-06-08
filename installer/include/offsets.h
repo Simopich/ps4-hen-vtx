22f34d2d543159b25f247c12d9f79ffb2e305f2a
Simone Ruggieri <simopich2002@gmail.com>
2024-06-08 11:56:03 +0200

    Added comments for automatically generated offsets

--- installer/include/offsets.h
+++ installer/include/offsets.h
@@ -6,12 +6,12 @@
 
 // Names - Data
 #define PRISON0_addr                    0x0111b840 // Done
-#define ROOTVNODE_addr                  0x021ebf20 // Done
-#define PMAP_STORE_addr                 0x01b8c4b0 // Done
+#define ROOTVNODE_addr                  0x021ebf20 // Done - Auto
+#define PMAP_STORE_addr                 0x01b8c4b0 // Done - Auto
 #define DT_HASH_SEGMENT_addr            0x00140fe0 // Done
 
 // Functions
-#define pmap_protect_addr               0x0012fa40 // Done
+#define pmap_protect_addr               0x0012fa40 // Done - Auto
 #define pmap_protect_p_addr             0x0012fa87 // Done
 #define malloc_addr                     0x003017B0 // Done
 
@@ -21,11 +21,11 @@
 #define debug_menu_error_patch2         0x004e802f // Done
 
 // disable signature check
-#define disable_signature_check_patch   0x00686580 // Done
+#define disable_signature_check_patch   0x00686580 // Done - Auto
 
 // enable debug RIFs
-#define enable_debug_rifs_patch1        0x0064e3f0 // Done
-#define enable_debug_rifs_patch2        0x0064e420 // Done
+#define enable_debug_rifs_patch1        0x0064e3f0 // Done - Auto
+#define enable_debug_rifs_patch2        0x0064e420 // Done - Auto
 
 // allow sys_dynlib_dlsym in all processes
 #define sys_dynlib_dlsym_patch          0x0023b34f // Done
@@ -34,9 +34,9 @@
 // #define sdk_version_patch               0x14A63F0
 
 // enable debug log
-#define enable_debug_log_patch          0x000b7ac7 // Done
+#define enable_debug_log_patch          0x000b7ac7 // Done - Auto
 
 // enable uart output
-#define enable_uart_patch               0x01527f60 // Done
+#define enable_uart_patch               0x01527f60 // Done - Auto
 
 #endif
--- kpayload/include/offsets.h
+++ kpayload/include/offsets.h
@@ -4,73 +4,73 @@
 
 // data
 #define	XFAST_SYSCALL_addr              0x000001C0 // Done
-#define M_TEMP_addr                     0x0155E1E0 // Done
+#define M_TEMP_addr                     0x0155E1E0 // Done - Auto
 #define MINI_SYSCORE_SELF_BINARY_addr   0x01575df8 // Done - Not 100% sure
-#define ALLPROC_addr                    0x01B906E0 // Done
-#define SBL_DRIVER_MAPPED_PAGES_addr    0x02642ca8 // Done
-#define SBL_PFS_SX_addr                 0x02649b40 // Done
-#define SBL_KEYMGR_KEY_SLOTS_addr       0x02644238 // Done
-#define SBL_KEYMGR_KEY_RBTREE_addr      0x02644248 // Done
-#define SBL_KEYMGR_BUF_VA_addr          0x02648000 // Done
-#define SBL_KEYMGR_BUF_GVA_addr         0x02648808 // Done
-#define FPU_CTX_addr                    0x026735c0 // Done
-#define DIPSW_addr                      0x02212850 // Done
+#define ALLPROC_addr                    0x01B906E0 // Done - Auto
+#define SBL_DRIVER_MAPPED_PAGES_addr    0x02642ca8 // Done - Auto
+#define SBL_PFS_SX_addr                 0x02649b40 // Done - Auto
+#define SBL_KEYMGR_KEY_SLOTS_addr       0x02644238 // Done - Auto
+#define SBL_KEYMGR_KEY_RBTREE_addr      0x02644248 // Done - Auto
+#define SBL_KEYMGR_BUF_VA_addr          0x02648000 // Done - Auto
+#define SBL_KEYMGR_BUF_GVA_addr         0x02648808 // Done - Auto
+#define FPU_CTX_addr                    0x026735c0 // Done - Auto
+#define DIPSW_addr                      0x02212850 // Done - Auto
 
 // common
-#define memcmp_addr                     0x00271aa0 // Done
-#define _sx_xlock_addr                  0x0043c530 // Done
-#define _sx_xunlock_addr                0x0043c6f0 // Done
-#define malloc_addr                     0x003017B0 // Done
-#define free_addr                       0x00301970 // Done
-#define strstr_addr                     0x004859b0 // Done
-#define fpu_kern_enter_addr             0x002193a0 // Done
-#define fpu_kern_leave_addr             0x00219460 // Done
-#define memcpy_addr                     0x00271130 // Done
-#define memset_addr                     0x00149670 // Done
-#define strlen_addr                     0x0030f0f0 // Done
-#define printf_addr                     0x000b79e0 // Done
-#define eventhandler_register_addr      0x000f8320 // Done
+#define memcmp_addr                     0x00271aa0 // Done - Auto
+#define _sx_xlock_addr                  0x0043c530 // Done - Auto
+#define _sx_xunlock_addr                0x0043c6f0 // Done - Auto
+#define malloc_addr                     0x003017B0 // Done - Auto
+#define free_addr                       0x00301970 // Done - Auto
+#define strstr_addr                     0x004859b0 // Done - Auto
+#define fpu_kern_enter_addr             0x002193a0 // Done - Auto
+#define fpu_kern_leave_addr             0x00219460 // Done - Auto
+#define memcpy_addr                     0x00271130 // Done - Auto
+#define memset_addr                     0x00149670 // Done - Auto
+#define strlen_addr                     0x0030f0f0 // Done - Auto
+#define printf_addr                     0x000b79e0 // Done - Auto
+#define eventhandler_register_addr      0x000f8320 // Done - Auto
 
 // Fself
-#define sceSblACMgrGetPathId_addr       0x0008bcd0 // Done
-#define sceSblServiceMailbox_addr       0x0062ec00 // Done
-#define sceSblAuthMgrSmIsLoadable2_addr 0x00641960 // Done
-#define _sceSblAuthMgrGetSelfInfo_addr  0x006421a0 // Done
-#define _sceSblAuthMgrSmStart_addr      0x0063dea0 // Done
-#define sceSblAuthMgrVerifyHeader_addr  0x0063fc20 // Done
+#define sceSblACMgrGetPathId_addr       0x0008bcd0 // Done - Auto
+#define sceSblServiceMailbox_addr       0x0062ec00 // Done - Auto
+#define sceSblAuthMgrSmIsLoadable2_addr 0x00641960 // Done - Auto
+#define _sceSblAuthMgrGetSelfInfo_addr  0x006421a0 // Done - Auto
+#define _sceSblAuthMgrSmStart_addr      0x0063dea0 // Done - Auto
+#define sceSblAuthMgrVerifyHeader_addr  0x0063fc20 // Done - Auto
 
 // Fpkg
 #define RsaesPkcs1v15Dec2048CRT_addr    0x004607b0 // Done
-#define Sha256Hmac_addr                 0x00442f80 // Done
-#define AesCbcCfb128Encrypt_addr        0x001ff000 // Done
-#define AesCbcCfb128Decrypt_addr        0x001ff230 // Done
-#define sceSblDriverSendMsg_0_addr      0x0061ae90 // Done
-#define sceSblPfsSetKeys_addr           0x00623290 // Done
-#define sceSblKeymgrSetKeyStorage_addr  0x00622930 // Done
-#define sceSblKeymgrSetKeyForPfs_addr   0x0061d650 // Done
-#define sceSblKeymgrCleartKey_addr      0x0061d990 // Done
-#define sceSblKeymgrSmCallfunc_addr     0x0061d220 // Done
+#define Sha256Hmac_addr                 0x00442f80 // Done - Auto
+#define AesCbcCfb128Encrypt_addr        0x001ff000 // Done - Auto
+#define AesCbcCfb128Decrypt_addr        0x001ff230 // Done - Auto
+#define sceSblDriverSendMsg_0_addr      0x0061ae90 // Done - Auto
+#define sceSblPfsSetKeys_addr           0x00623290 // Done - Auto
+#define sceSblKeymgrSetKeyStorage_addr  0x00622930 // Done - Auto
+#define sceSblKeymgrSetKeyForPfs_addr   0x0061d650 // Done - Auto
+#define sceSblKeymgrCleartKey_addr      0x0061d990 // Done - Auto
+#define sceSblKeymgrSmCallfunc_addr     0x0061d220 // Done - Auto
 
 // Patch
-#define vmspace_acquire_ref_addr        0x0007b9e0 // Done
-#define vmspace_free_addr               0x0007b810 // Done
-#define vm_map_lock_read_addr           0x0007BB80 // Done
-#define vm_map_unlock_read_addr         0x0007BBD0 // Done
-#define vm_map_lookup_entry_addr        0x0007C1C0 // Done
-#define proc_rwmem_addr                 0x0041ca70 // Done
+#define vmspace_acquire_ref_addr        0x0007b9e0 // Done - Auto
+#define vmspace_free_addr               0x0007b810 // Done - Auto
+#define vm_map_lock_read_addr           0x0007BB80 // Done - Auto
+#define vm_map_unlock_read_addr         0x0007BBD0 // Done - Auto
+#define vm_map_lookup_entry_addr        0x0007C1C0 // Done - Auto
+#define proc_rwmem_addr                 0x0041ca70 // Done - Auto
 
 // Fself hooks
-#define sceSblAuthMgrIsLoadable__sceSblACMgrGetPathId_hook          0x006426fc // Done
+#define sceSblAuthMgrIsLoadable__sceSblACMgrGetPathId_hook          0x006426fc // Done - Auto
 #define sceSblAuthMgrIsLoadable2_hook                               0x0064284e // Done
-#define sceSblAuthMgrVerifyHeader_hook1                             0x00642fe6 // Done
+#define sceSblAuthMgrVerifyHeader_hook1                             0x00642fe6 // Done - Auto
 #define sceSblAuthMgrVerifyHeader_hook2                             0x00643cc9 // Done
-#define sceSblAuthMgrSmLoadSelfSegment__sceSblServiceMailbox_hook   0x006402ea // Done
+#define sceSblAuthMgrSmLoadSelfSegment__sceSblServiceMailbox_hook   0x006402ea // Done - Auto
 #define sceSblAuthMgrSmLoadSelfBlock__sceSblServiceMailbox_hook     0x00640f28 // Done
 
 // Fpkg hooks
-#define sceSblKeymgrSetKeyStorage__sceSblDriverSendMsg_hook         0x006229d5 // Done
+#define sceSblKeymgrSetKeyStorage__sceSblDriverSendMsg_hook         0x006229d5 // Done - Auto
 #define sceSblKeymgrInvalidateKey__sx_xlock_hook                    0x0061E80D // Done
-#define sceSblKeymgrSmCallfunc_npdrm_decrypt_isolated_rif_hook      0x0064c030 // Done
+#define sceSblKeymgrSmCallfunc_npdrm_decrypt_isolated_rif_hook      0x0064c030 // Done - Auto
 #define sceSblKeymgrSmCallfunc_npdrm_decrypt_rif_new_hook           0x0064CDFE // Done
 #define mountpfs__sceSblPfsSetKeys_hook1                            0x006C1EB9 // Done
 #define mountpfs__sceSblPfsSetKeys_hook2                            0x006C20EA // Done
